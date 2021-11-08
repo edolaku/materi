b0efeef1c54d0b65065b21d5ba5c93ee9e935630
edolaku <edoooq@gmail.com>
2021-11-09 06:46:58 +0700

    Update materi php tentang pengulangan pada latihan1.php

---
+++ php/latihan1.php
@@ -0,0 +1,25 @@
+<?php
+//PENGULANGAN
+
+//for
+for ($i = 0; $i < 5; $i++){
+    echo "syntax for <br>";
+}
+
+// while
+$i = 0;
+while ($i <= 10) {
+    echo "syntax while <br>";
+    $i++;
+}
+
+
+// do..while
+// untuk syntax do..while, ketika kondisinya bernilai false, perintahnya akan dijalankan dulu sekali
+$i = 5;
+do {
+    echo "syntax do..while <br>";
+    $i++; 
+} while ($i <= 10);
+
+?>
