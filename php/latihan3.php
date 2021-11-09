b5e1b28c8973a2f0518c3139a089d13419887440
edolaku <edoooq@gmail.com>
2021-11-09 15:36:16 +0700

    update materi pengkondisian php

--- php/latihan3.php
+++ php/latihan3.php
@@ -6,6 +6,11 @@
     <meta content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pengkondisian PHP</title>
+    <style>
+        .warna {
+            background-color: salmon;
+        }
+    </style>
 </head>
 
 <body>
@@ -16,9 +21,37 @@
     // #2 if else if else
     // #3 ternary
     // #4 switch
+
+
+
     ?>
     <h1>PENGKONDISIAN/PERCABANGAN PHP</h1>
 
+    <?php
+    $i = 20;
+    if ($i < 20) {
+        echo "benar";
+    } else if ($i == 20) {
+        echo "bingo!";
+    } else {
+        echo "salah";
+    }
+    ?>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <caption>table dg PHP template</caption>
+        <?php for ($i = 1; $i <= 5; $i++) : ?>
+            <!-- warnai baris yg genap dengan pengkondisian -->
+            <?php if ($i % 2 == 0) : ?>
+                <tr class="warna">
+                <?php else : ?>
+                <tr>
+                <?php endif ?>
+                <?php for ($j = 1; $j <= 5; $j++) : ?>
+                    <td> <?= "$i,$j"; ?> </td>
+                <?php endfor; ?>
+                </tr>
+            <?php endfor; ?>
 </body>
 
 </html>
